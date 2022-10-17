6ff6b151ced6bdb986f1aef5b4c40ac8cedcad9f
David <dcounagogonzalez@danielcastelao.org>
2022-10-17 14:42:04 +0200

    Ejemplos de selects

--- exemploBD.py
+++ exemploBD.py
@@ -16,7 +16,42 @@ try:
 
     c.execute('''create table usuarios (dni text,
                                         nome text,
-                                        direccion text''')
+                                        direccion text)''')
+
+    c.execute('''insert into usuarios values ('3333-A', 'Maria', 'Canceleiro') ''')
+
+    c.execute('''insert into usuarios values ('4444-B', 'Manuel', 'Rosalia') ''')
+
+    c.execute('''insert into usuarios values ('5555-C', 'Ana', 'Areal') ''')
+
+    bbdd.commit()
 
 except dbapi.DatabaseError as e:
-    print("Erro na BD" + str(e))
+    print("Erro na BD " + str(e))
+
+try:
+    c.execute('''select dni, nome, direccion from usuarios''')
+
+    print(type(c.fetchall()))
+    for rexistro in c.fetchall():
+        print("Nome: " + rexistro[1])
+
+except dbapi.DatabaseError as e:
+    print("Erro na BD " +  str(e))
+
+try:
+    """Non Facer ASÍ!!!
+    dni=input("Introduce o dni: ")
+    c.execute('''select dni, nome, direccion from usuarios where dni="'''+ dni + '"')"""
+
+
+    dni = input("Introduce o dni: ")
+    c.execute('''select dni, nome, direccion from usuarios where dni=?''', (dni,) )
+
+    for rexistro in c.fetchall():
+        print("DNI: "+ rexistro[0])
+        print("Nome: " + rexistro[1])
+        print("Direccion: " + rexistro[2])
+
+except dbapi.DatabaseError as e:
+    print("Erro na BD " + str(e))
